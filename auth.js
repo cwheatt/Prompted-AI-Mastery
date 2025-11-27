44989c6902db04cdac49c6df835aace5a4669401
dpearyer <dpearyer@users.noreply.github.com>
2025-11-27 18:56:50 +0000

    deploy: 3ff44eb41b75bd770a6c96db69387239fe504ce3

--- auth.js
+++ auth.js
@@ -1,1 +1,1 @@
-window.supabase.createClient(void 0,void 0);
+console.log("Supabase URL:",void 0);console.log("Has anon key:",!1);throw console.error("Missing Supabase environment variables"),new Error("Supabase configuration is missing");
