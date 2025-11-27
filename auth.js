69c22c96c0346d89c3c383076f78629f1234534f
dpearyer <dpearyer@users.noreply.github.com>
2025-11-27 19:24:28 +0000

    deploy: 01463061a06ca7fb539a30e8ebd833a035678c5a

--- auth.js
+++ auth.js
@@ -1,1 +1,6 @@
-console.log("Supabase URL:",void 0);console.log("Has anon key:",!1);throw console.error("Missing Supabase environment variables"),new Error("Supabase configuration is missing");
+const supabaseUrl = "https://tisxbfdljpjyptrlbrkb.supabase.co";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRpc3hiZmRsanBqeXB0cmxicmtiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjI2NjYzMzcsImV4cCI6MjA3ODI0MjMzN30.ToLI0L-CihtcNNZnr7tY1JxO-fGgPZKsIdet1ScJbOY";
+
+console.log('Supabase initialized');
+
+export const supabase = window.supabase.createClient(supabaseUrl, supabaseAnonKey);
